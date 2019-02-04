bde9746e189b92b54714f373b8fe4040ddd68356
Kevin Choi <kevinyoungchoi@gmail.com>
2019-02-04 16:38:42 -0500

    controlflowconfusing

---
+++ controlflow.js
@@ -0,0 +1,29 @@
+const LIBRARY_NAME = 'library'
+let book = ["Dong Quixote", "Ulysses", "The Odyssey", "War and Peace", "Moby Dick", "Hamlet", "The Great Gatsby"]
+let author = ["Miguel de Cervantes", "James Joyce", "Homer", "Leo Tolstoy", "Herman Melville", "William Shakespeare", "null"]
+let pages = [ 300, 450, 232, 578, 321, " ", 450 ]
+
+const buildLibrary = (book, author)
+  return {
+    book,
+    author,
+    pages
+  }
+const buildBOOK_LIST
+const createBook = (name, author, pages, isRead) => {
+  forEach
+}
+const book = bookFactory('Don Quixote', 'Miguel de Cervantes', 300, true)
+const BOOK_LIST = [
+    ["Don Quixote", "Miguel de Cervantes", 300, true],
+    ["Ulysses", "James Joyce", 450, true],
+    ["The Odyssey", "Homer", 232],
+    ["War and Peace", "Leo Tolstoy", 578],
+    ["Moby Dick", "Herman Melville", 321],
+    ["Hamlet", "William Shakespeare"],
+    ["The Great Gatsby", null, 450]
+  ]
+  return { 'name', 'author', 'pages', 'isRead' };
+};
+
+console.log(...book.name);
---
+++ variableanddatatypes.js
@@ -0,0 +1,4 @@
+const LIBRARY_NAME = 'library'
+let myBooks = 'books'
+console.log('library');
+console.log('books');
